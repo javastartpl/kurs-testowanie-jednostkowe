177b31be6eb69554bccba2548ae32c1834b93763
Marcin Kunert <the.martines@gmail.com>
2020-06-25 15:39:32 +0200

    Zadanie z koszykiem wip

---
+++ exercises/src/main/java/pl/javastart/tj/shoppingcart/ShoppingCartCalculatorService.java
@@ -0,0 +1,10 @@
+package pl.javastart.tj.shoppingcart;
+
+public class ShoppingCartCalculatorService {
+
+    public ShoppingCartDto calculateShoppingCartPrice(ShoppingCartDto shoppingCartDto) {
+        // wylicz ceny i zwróć uzupelnione ShoppingCartDto
+        return shoppingCartDto;
+    }
+
+}
---
+++ exercises/src/main/java/pl/javastart/tj/shoppingcart/ShoppingCartDto.java
@@ -0,0 +1,4 @@
+package pl.javastart.tj.shoppingcart;
+
+public class ShoppingCartDto {
+}
